5fe6566159f14c576b20d13db281d1b005163c9b
winn <myemail@em.com>
2024-01-24 23:10:14 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    var totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var movingElement = document.getElementById('movingElement');
+    var movingIcon = this.document.querySelector('#movingElement img')
+
+    // Calculate the percentage of page scrolled
+    var translateY = (scrollPosition / totalHeight) * 100;
+    movingElement.style.transform = 'translateY(' + translateY + 'svh)';
+    if(translateY < 25) {
+        movingIcon.src = "./images/sunrise.png";
+        
+    } else if (translateY >= 25 && translateY < 50) {
+        movingIcon.src = "./images/sun.png";
+    } 
+    else if (translateY >= 50 && translateY < 75) {
+        movingIcon.src = "./images/sunrise.png";
+    }
+    else {
+        movingIcon.src = "./images/moon.png";
+    }
+});
+
